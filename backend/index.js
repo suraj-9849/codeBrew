58e2258659df85bd0fa33f81ac0d7f37333f908e
GUNUPURU SURAJ <146669517+suraj-9849@users.noreply.github.com>
2024-08-08 23:29:53 +0530

    Added the createPortfolio Page

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,44 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors());
+app.use(express.json());
+
+let userData = {};
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/userData", (req, res) => {
+  if (Object.keys(userData).length) {
+    res.status(200).json({
+      msg: userData,
+    });
+  } else {
+    res.status(404).json({
+      msg: "No user data found",
+    });
+  }
+});
+
+app.post("/userData", (req, res) => {
+  userData = req.body;
+
+  if (userData) {
+    res.status(200).json({
+      msg: "User data received successfully",
+      data: userData,
+    });
+  } else {
+    res.status(400).json({
+      msg: "Error Occurred in receiving the user data",
+    });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
